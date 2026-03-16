63ad7e8bf3576906e5da2703fca3d7b74b1c1cf6
Sahil-01-crypto <sahil.pihul2007@gmail.com>
2026-03-16 17:52:15 +0530

    solved

--- _stock_span.java
+++ _stock_span.java
@@ -1,27 +1,36 @@
 package leetcode;
 
 import java.util.Stack;
-
+class Pair{
+    int data ;
+    int index ;
+    Pair( int data  , int index) {
+        this.data = data;
+        this.index = index;
+    }
+}
 public class _stock_span {
     public  static  int calcStockSpan( int [] arr){
-        int n = arr . length ;
-        Stack<Integer> st = new Stack<>();
-        int maxi =1 ;
+        Stack<Pair> st = new Stack<>();
+        int ans = 1 ;
         for( int i = 0;i<arr.length ; i++){
-            int  count = 0;
-            while(!st.isEmpty() && arr[st.peek()]<= arr[i]){
-                st.pop();
+            int count  ;
+            int curr = arr[i];
+            while( !st.isEmpty() && st.peek(). data <= curr ){
+                st . pop();
             }
             if( st.isEmpty()){
-                count+=i+1;
+                count = i+1 ;
+
             }
             else{
-                count += i-st.peek();
+                count=  i - st.peek().index;
             }
-            maxi = Math.max(maxi , count);
-            st . push(i);
+            ans = Math . max(count , ans);
+            st.push( new Pair(arr[i] , i));
         }
-        return  maxi ;
+        return  ans ;
+
     }
     public static void main(String[] args) {
         int  stock[]={7,2,1,3,3,1,8};
